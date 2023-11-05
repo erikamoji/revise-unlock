69dbb2ff0bb39dd167ff095409aae832168cdf82
erikamoji <ekpnguyen@gmail.com>
2023-11-05 12:47:03 +0700

    frontend and mock data

--- lib/unlockPoll.ts
+++ lib/unlockPoll.ts
@@ -1,25 +1,35 @@
-import dotenv from 'dotenv';
-import Web3 from 'web3';
+// import dotenv from 'dotenv';
+// import Web3 from 'web3';
 import nodemailer from 'nodemailer';
-import { LockManagerABI } from './LockManagerABI';
 
-// Load environment variables from .env.local file
-dotenv.config();
+// Comment out code that would be used in a real-world deployment (this is currently using mock info)
 
-// Now you can use process.env to access your environment variables:
-const infuraProjectId = process.env.INFURA_PROJECT_ID;
+// import { LockManagerABI } from './LockManagerABI';
+// dotenv.config(); // Load environment variables from .env.local file
+// const infuraProjectId = process.env.INFURA_PROJECT_ID; // Now you can use process.env to access your environment variables
+// const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${infuraProjectId}`)); // Step 1: Instantiate web3 instance connected to a provider
+// Step 2: Create contract instance (this would be used in the actual deployment)
+// const lockManagerAddress = "0x226124E83868812D3Dae87eB3C5F28047E1070B7";
+// const lockManagerContract = new web3.eth.Contract(LockManagerABI, lockManagerAddress); 
 
-// Step 1: Instantiate web3 instance connected to a provider
-const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${infuraProjectId}`));
+// This code snippet uses a mockFNFTData object to simulate the maturity status of FNFTs.
 
-// Step 2: Create contract instance
-const lockManagerAddress = "0x226124E83868812D3Dae87eB3C5F28047E1070B7";
-const lockManagerContract = new web3.eth.Contract(LockManagerABI, lockManagerAddress);
+// Mock FNFT data
+const mockFNFTData = {
+    'fnft-1': { mature: false },
+    'fnft-2': { mature: true },
+    // ... more mock data as needed
+};
 
-const fnftId = /* ...the FNFT ID you are interested in... */;
+// The getMockLockMaturity function pretends to call a smart contract and returns the maturity status of an FNFT based on the mock data.
+async function getMockLockMaturity(fnftId) {
+    // Simulate a delay to mimic async behavior of blockchain calls
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    return mockFNFTData[fnftId]?.mature || false;
+}
 
 // Function to send email notification
-async function sendNotification() {
+async function sendNotification(fnftId) {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
@@ -43,14 +53,17 @@ async function sendNotification() {
     }
 }
 
-// Function to start polling the contract
-function startPolling() {
+// The startPollingWithMock function uses this mock function instead of the actual smart contract call.
+function startPollingWithMock() {
     const intervalId = setInterval(async () => {
         try {
-            const mature = await lockManagerContract.methods.getLockMaturity(fnftId).call();
+            // Use mock function instead of smart contract call
+            const fnftId = 'fnft-1';  // Replace with dynamic ID as needed
+            const mature = await getMockLockMaturity(fnftId);
             if (mature) {
                 // If mature, send notification and clear interval
-                sendNotification();
+                console.log(`FNFT ${fnftId} has matured!`);
+                await sendNotification(fnftId);
                 clearInterval(intervalId);
             }
         } catch (error) {
@@ -61,5 +74,5 @@ function startPolling() {
     return intervalId;  // Return interval ID to allow for clearing later if needed
 }
 
-// Start polling when file is run
-startPolling();
+// Start polling with mock data when file is run
+startPollingWithMock();
